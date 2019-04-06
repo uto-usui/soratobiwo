cb831226f68f19e727afd37e621c3127b964a5b4
uto-usui <me@uto-usui.com>
2019-04-07 02:46:17 +0900

    ✨ Add Hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,9 @@
 <template>
-  <div
-    class="hero"
-  >
-    hero
+  <div class="hero">
+    <div
+      v-lazy:background-image="require('Images/hero/item2.jpg')"
+      class="hero__main"
+    />
   </div>
 </template>
 
@@ -22,7 +23,6 @@ export default {
   },
   data() {
     return {
-      videoSelected: false,
       height: '',
     }
   },
@@ -46,5 +46,34 @@ export default {
 <style lang="scss" scoped>
 .hero {
   //
+}
+
+.hero__main {
+  position: fixed;
+  top: calc(100vw * 20 / 375);
+  left: 0;
+  width: calc(100vw * 300 / 375);
+  height: calc(100vh * 200 / 667);
+  //
+  @include bg-cover;
+  //
+  @include desktop {
+    top: calc(100vw * 30 / 1024);
+    width: calc(100vw - 100vw * 30 / 1024);
+    height: calc(100vh * 482 / 768);
+  }
+  //
+  &::after {
+    //
+    @include touch {
+      position: absolute;
+      top: 100%;
+      right: 0;
+      left: 0;
+      height: calc(100vw * 20 / 375);
+      content: '';
+      background-color: $color-gray-level2;
+    }
+  }
 }
 </style>
