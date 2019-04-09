6251d2c16644e33f1494e97cd53bbfe82e82c025
uto-usui <me@uto-usui.com>
2019-04-09 10:33:51 +0900

    ✨ Add Main components

---
+++ components/MainInner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-inner">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainInner',
+  props: {
+    //
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main-inner {
+  padding: calc(100vw * 30 / 375);
+  background-color: $color-gray-level1;
+  //
+  @include desktop {
+    padding: calc(100vw * 30 / 1024);
+  }
+}
+</style>
---
+++ components/MainOuter.vue
@@ -0,0 +1,20 @@
+<template>
+  <article class="main-outer">
+    <slot />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'MainOuter',
+  props: {
+    //
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main-outer {
+  width: calc(100vw / 3 * 2);
+}
+</style>
