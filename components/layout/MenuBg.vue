dcfa6bd6f24f75a7e5f773f3b829cb5ec6209b6f
uto-usui <me@uto-usui.com>
2019-04-10 20:24:22 +0900

    ✨ Add MenuBg

---
+++ components/layout/MenuBg.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="[{ 'is-open': getMenuOpen }]" class="menu-bg"></div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBg',
+  props: {
+    getMenuOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-bg {
+  position: fixed;
+  top: calc(100vw * 20 / 375);
+  right: calc(100vw * 20 / 375);
+  bottom: 50px;
+  left: 0;
+  z-index: $z-menu-bg;
+  pointer-events: none;
+  background-color: rgba($color-white, 0.9);
+  opacity: 0;
+  //
+  &.is-open {
+    pointer-events: auto;
+    opacity: 1;
+  }
+}
+</style>
