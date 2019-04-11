9fc887ba5f3f04274695b6c84d942d2b12fec75b
uto-usui <me@uto-usui.com>
2019-04-12 01:05:12 +0900

    ✨ Add news detail page

---
+++ pages/news/_id.vue
@@ -0,0 +1,74 @@
+<template>
+  <Wrapper class="live" :get-page-ready="getPageReady">
+    <MainOuter>
+      <MainInner>
+        <Article :get-is-desktop="getIsDesktop" :post="post" />
+        <ArticleBottom :image="require('Images/hero/item10.jpg')" />
+      </MainInner>
+    </MainOuter>
+  </Wrapper>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+import Wrapper from '~/components/layout/Wrapper'
+import MainInner from '~/components/MainInner'
+import MainOuter from '~/components/MainOuter'
+
+import Article from '~/components/Article'
+import ArticleBottom from '~/components/ArticleBottom'
+
+import HeadMixin from '~/mixins/Head'
+import newsData from '~/assets/json/news'
+
+import { pause } from 'Js/animation'
+
+export default {
+  components: {
+    Wrapper,
+    MainInner,
+    MainOuter,
+    Article,
+    ArticleBottom,
+  },
+  mixins: [HeadMixin],
+  data() {
+    return {
+      post: {},
+      title: '',
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getPageReady',
+      'getPageHeight',
+      'getPageWidth',
+      'getPageScrollY',
+      'getMenuOpen',
+      'getIsDesktop',
+    ]),
+  },
+  asyncData({ params }) {
+    const post = newsData.find(post => `${post.id}` === params.id)
+
+    if (post) {
+      return {
+        post,
+        title: post.title,
+      }
+    } else {
+      return { statusCode: 404, message: 'Post not found' }
+    }
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      await pause(1)
+      this.setPageReady(true)
+    })
+  },
+  methods: {
+    ...mapActions(['setPageReady']),
+  },
+}
+</script>
