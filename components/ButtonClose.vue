67b5edf497605461e47470dc861d6a1186252424
uto-usui <me@uto-usui.com>
2019-04-12 00:10:24 +0900

    ✨ Add close button

--- components/ButtonClose.vue
+++ components/ButtonClose.vue
@@ -1,50 +1,35 @@
 <template>
-  <nuxt-link to="/" class="button-close" />
+  <n-link to="/" class="button-close" />
 </template>
 
 <style lang="scss" scoped>
 .button-close {
-  position: absolute;
-  top: 15px;
-  right: 25px;
+  position: relative;
   display: block;
-  width: 25px;
-  height: 25px;
-  color: inherit;
-
-  @include desktop {
-    top: -10px;
-    right: 0;
-  }
+  width: 30px;
+  height: 30px;
+  background-color: $color-red;
 
   &::before {
     position: absolute;
     top: 50%;
-    right: 0;
+    right: 50%;
     width: 25px;
     height: 0;
     content: '';
-    border-top: solid 1px currentColor;
-    transform: rotate(45deg);
-
-    @include desktop {
-      width: 35px;
-    }
+    border-top: solid 1px $color-gray-level1;
+    transform: translateX(50%) rotate(45deg);
   }
 
   &::after {
     position: absolute;
     top: 50%;
-    right: 0;
+    right: 50%;
     width: 25px;
     height: 0;
     content: '';
-    border-top: solid 1px currentColor;
-    transform: rotate(-45deg);
-
-    @include desktop {
-      width: 35px;
-    }
+    border-top: solid 1px $color-gray-level1;
+    transform: translateX(50%) rotate(-45deg);
   }
 }
 </style>
