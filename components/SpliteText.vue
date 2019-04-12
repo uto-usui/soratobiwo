1ff1b1a440e65a3359c44fb118df1fdea965d627
uto-usui <me@uto-usui.com>
2019-04-13 04:28:26 +0900

    ✨ Add SpliteText

---
+++ components/SpliteText.vue
@@ -0,0 +1,50 @@
+<template>
+  <span
+    class="sprite-text"
+    :style="{
+      animationDelay: `${delay}s`,
+      animationDuration: `${duration}s`,
+    }"
+    v-text="item"
+  />
+</template>
+
+<script>
+export default {
+  name: 'SpriteText',
+  components: {},
+  props: {
+    duration: {
+      type: Number,
+      default: 0,
+    },
+    delay: {
+      type: Number,
+      default: 0,
+    },
+    index: {
+      type: Number,
+      default: 0,
+    },
+    item: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      //
+    }
+  },
+  computed: {
+    //
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.sprite-text {
+  display: inline-block;
+  backface-visibility: hidden;
+}
+</style>
