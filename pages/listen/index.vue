b3dd2ba56b366351a99081f98ca57b6f685e937a
uto-usui <me@uto-usui.com>
2019-04-12 23:12:29 +0900

    ✨ Add listen page

---
+++ pages/listen/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <Wrapper class="follow" :get-page-ready="getPageReady">
+    <MainOuter>
+      <HeroSub
+        :image="require('Images/hero/item15.jpg')"
+        :title="`listen`"
+        :get-page-ready="getPageReady"
+      />
+      <MainInner>
+        <FollowList :list-data="listenData" />
+      </MainInner>
+    </MainOuter>
+  </Wrapper>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+import Wrapper from '~/components/layout/Wrapper'
+import MainInner from '~/components/MainInner'
+import MainOuter from '~/components/MainOuter'
+import HeroSub from '~/components/HeroSub'
+import FollowList from '~/components/FollowList'
+
+import listenData from '~/assets/json/listen'
+
+import HeadMixin from '~/mixins/Head'
+
+import { pause } from 'Js/animation'
+
+export default {
+  components: {
+    Wrapper,
+    HeroSub,
+    FollowList,
+    MainInner,
+    MainOuter,
+  },
+  mixins: [HeadMixin],
+  data() {
+    return {
+      listenData,
+      title: 'listen|soratobiwo',
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getPageReady',
+      'getPageHeight',
+      'getPageWidth',
+      'getPageScrollY',
+      'getMenuOpen',
+    ]),
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      await pause(1)
+      this.setPageReady(true)
+    })
+  },
+  methods: {
+    ...mapActions(['setPageReady']),
+  },
+}
+</script>
