b00da347abd50711bcf71804ce458ee6cfe5f6ef
uto-usui <me@uto-usui.com>
2019-04-14 04:54:47 +0900

    ✨ Add thanks page

---
+++ layouts/thanks.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    :class="[
+      { 'is-ready': getPageReady },
+      { 'is-ie': $ua.browser() === 'Internet Explorer' },
+    ]"
+    class="body"
+  >
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$nextTick(() => {
+      //
+    })
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.body {
+  min-height: 100vh;
+  background-color: $color-primary;
+}
+</style>
+
+<style lang="scss">
+@import '~Sass/foundation/_reset';
+@import '~Sass/foundation/base/_base';
+@import '~Sass/animation/_keyframes';
+@import '~Sass/object/utility/_utility';
+
+@font-face {
+  font-family: 'MyYuGothicM';
+  font-weight: normal;
+  src: local('YuGothic-Medium'), local('Yu Gothic Medium'),
+    local('YuGothic-Regular');
+}
+
+@font-face {
+  font-family: 'MyYuGothicM';
+  font-weight: bold;
+  src: local('YuGothic-Bold'), local('Yu Gothic');
+}
+</style>
---
+++ pages/thanks/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="thanks">
+    <div class="">
+      <h1 class="thanks__title">thanks !</h1>
+      <p class="thanks__link-wrap">
+        <a href="../" class="thanks__link">go to home -></a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.thanks {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.thanks__title {
+  margin-bottom: 15px;
+  font-family: $font-Nunito;
+  font-size: 10vw;
+  font-weight: $font-Nunito-bold;
+  color: $color-gray-level1;
+  text-transform: uppercase;
+}
+
+.thanks__link {
+  font-size: 5vw;
+  font-weight: $font-Nunito-bold;
+  color: $color-gray-level1;
+  text-transform: uppercase;
+  border-bottom: 5px solid currentColor;
+  //
+  @include desktop {
+    font-size: 3vw;
+  }
+}
+</style>
+
+<script>
+import HeadMixin from '~/mixins/Head'
+
+export default {
+  layout: 'thanks',
+  components: {
+    //
+  },
+  mixins: [HeadMixin],
+  data() {
+    return {
+      title: 'thanks',
+    }
+  },
+}
+</script>
