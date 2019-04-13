388ce6f1cc59ac9a7563773cf55d32a387ce0433
uto-usui <me@uto-usui.com>
2019-04-13 14:54:31 +0900

    💄 Update contact bar style

--- components/layout/Contact.vue
+++ components/layout/Contact.vue
@@ -48,7 +48,26 @@ export default {
 }
 
 .contact__bar {
+  position: relative;
   height: 10px;
   background-color: $color-gray-level1;
+  //
+  &::after {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: calc(100vw * 20 / 375);
+    content: '';
+    background-color: $color-red;
+    //
+    @include desktop {
+      width: calc(100vw * 30 / 1024);
+    }
+    //
+    @include widescreen {
+      width: calc(100vw * 30 / 1280);
+    }
+  }
 }
 </style>
