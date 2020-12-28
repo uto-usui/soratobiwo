abdff6b7429da366840cbec69f7478fe8dd33b20
uto-usui <nattyanoranged@gmail.com>
2020-12-28 21:39:56 +0900

    💬 Update news data

--- plugins/vue-lazyload.client.js
+++ plugins/vue-lazyload.client.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
 
 Vue.use(VueLazyload, {
+  preLoad: 1.5,
+  attempt: 1,
   observer: true,
   observerOptions: {
-    rootMargin: '0px',
-    threshold: 0.1,
+    rootMargin: '500px 500px 500px 500px',
+    threshold: 0,
   },
 })
